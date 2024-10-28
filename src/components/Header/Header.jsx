076bda9e7cb2e61e4e19bf95a4e021f77a534fc0
bekich-2010 <you@example.com>
2024-10-28 16:01:15 +0600

    орхан черт удалил header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,53 +1,32 @@
 import React, {useState} from 'react';
 import './Header.scss'
-import {IoIosArrowDown, IoIosArrowUp} from "react-icons/io";
-import { MdFavoriteBorder } from "react-icons/md";
-import { FiShoppingCart } from "react-icons/fi";
+import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
 
 const Header = () => {
 
-    const [showPhone, setShowPhone] = useState(false)
     const [active, setActive] = useState(false)
-    const dropChoise = () => {
-        setShowPhone(!showPhone);
-    }
-    const activeArrow = () => {
-        setActive(active => !active);
-    }
 
+    const Arrows = () => {
+        setActive(!active)
+    }
     return (
-        <header className="header">
+        <haeder className="header">
             <div className="container">
-                <div className="header__nav-div">
-                    <nav className="header__nav">
-                        <h1 className="header__title">QPICK</h1>
-                        <p onClick={() => setShowPhone(dropChoise)} className="header__choise">Выбрать модель
-                            телефона
-                            <span className="header__nav-arr">
-                            {
-                                active ? <IoIosArrowUp/>
-                                    : <IoIosArrowDown/>
-                            }
-                        </span>
-                            <ul style={showPhone ? {display: "inline"} : {display: "none"}}
-                                className="header__list">
-                                <li className="header__link">iphone 12</li>
-                                <li className="header__link">iphone 12</li>
-                                <li className="header__link">iphone 12</li>
-                                <li className="header__link">iphone 12</li>
-                                <li className="header__link">iphone 12</li>
-                            </ul>
+                <h2 className="header__title">QPICK</h2>
+                <div className="header__nav">
+                    <nav className="header__nav-div">
+                        <p className="header__nav-choise">
+                            Выбрать модель телефона
+                            <span className="header__nav-sapn">
+                                {
+                                    Arrows
+                                }
+                            </span>
                         </p>
-
-
                     </nav>
-                    <div className="header__cart-fav">
-                        <MdFavoriteBorder/>
-                        <FiShoppingCart/>
-                    </div>
                 </div>
             </div>
-        </header>
+        </haeder>
     );
 };
 
